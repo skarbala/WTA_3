2967a4b35f3de3097814c5d5d563055ffebc8f32
martin.skarbala <skarbala.martin@gmail.com>
2018-08-13 14:51:08 +0200

    added two more methods for calculator

--- kalkulacka.php
+++ kalkulacka.php
@@ -4,60 +4,68 @@
 <link href="https://fonts.googleapis.com/css?family=Press+Start+2P" rel="stylesheet">
 
 <style>
-    input {
-        margin-bottom: 10px;
-    }
+  input {
+    margin-bottom: 10px;
+  }
 
-    button.btn {
-        margin-bottom: 20px;
-    }
+  button.btn {
+    margin-bottom: 20px;
+  }
 
-    body {
-        background-color: #fdfdfd;
-    }
+  body {
+    background-color: #fdfdfd;
+  }
 
-    h1 {
-        font-family: 'Press Start 2P', serif;
-        font-size: 40px;
-        margin-bottom: 50px;
-        color: #4d4d4d;
-        text-shadow: 2px 2px 1px #a3a4a6;
-    }
+  h1 {
+    font-size: 40px;
+    margin-bottom: 50px;
 
-    button.btn, input.form-control {
-        border-radius: 0;
-    }
+  }
 
-    button.btn {
-        margin: 0;
-    }
+  h1, h2 {
+    font-family: 'Press Start 2P', serif;
+    color: #4d4d4d;
+    text-shadow: 2px 2px 1px #a3a4a6;
+  }
 
-    #result {
-        margin: 30px 0;
-    }
+  button.btn, input.form-control {
+    border-radius: 0;
+  }
+
+  button.btn {
+    margin: 0;
+  }
+
+  #result {
+    margin: 30px 0;
+  }
 
 </style>
 <body>
 <?php include 'partials/navigation.php' ?>
 <div class="container">
-    <div class="row">
-        <h1 class="text-center">Kalkulacka</h1>
-        <div class="col-md-6 col-md-offset-3">
-            <div>
-                <div>
-                    <input type="text" class="form-control" id="firstInput" placeholder="prve cislo">
-                </div>
-                <div>
-                    <input type="text" class="form-control" id="secondInput" placeholder="druhe cislo">
-                </div>
-                <button id="count" class="btn btn-success btn-block">Spocitaj</button>
-                <button id="deduct" class="btn btn-danger btn-block">Odpocitaj</button>
-              <button id="multiply" class="btn btn-primary btn-block">Vynasob</button>
-                <button id="reset" class="btn btn-default btn-block">Vynuluj</button>
-            </div>
-            <h1 class="text-center" id="result"></h1>
+  <div class="row">
+    <h1 class="text-center">Kalkulacka</h1>
+    <div class="col-md-6 col-md-offset-3">
+      <div>
+        <div>
+          <input type="text" class="form-control" id="firstInput" placeholder="prve cislo">
         </div>
+        <div>
+          <input type="text" class="form-control" id="secondInput" placeholder="druhe cislo">
+        </div>
+        <button id="count" class="btn btn-success btn-block">Spocitaj</button>
+        <button id="deduct" class="btn btn-danger btn-block">Odpocitaj</button>
+        <button id="multiply" class="btn btn-primary btn-block">Vynasob</button>
+        <button id="divide" class="btn btn-warning btn-block">Vydel</button>
+        <button id="reset" class="btn btn-default btn-block">Vynuluj</button>
+      </div>
+      <div class="text-center">
+        <h1>Výsledok</h1>
+        <h2 id="result"></h2>
+      </div>
     </div>
+  </div>
 </div>
 </body>
 <script>
@@ -100,6 +108,14 @@
         });
         $("#multiply").click(function () {
             multiply();
+        });
+
+        $("#divide").click(function () {
+            validateInput();
+            validateSecondInput();
+            if (validateInput() && validateSecondInput()) {
+                $("#result").text(parseInt($("#firstInput").val()) / parseInt($("#secondInput").val()));
+            }
         });
 
         $("#reset").click(function () {
